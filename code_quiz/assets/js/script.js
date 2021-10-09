94c5d76689efde39c94942d4af803e2ee47e274e
Your Name <minhakim@mins-mbp.home>
2021-10-09 13:03:27 -0400

    update

---
+++ code_quiz/assets/js/script.js
@@ -0,0 +1,136 @@
+var questions = [
+    {
+        question: "1.Waht does HTML stand for",
+        answers: {
+            a: "Hyperlinks and Text Markup Language",
+            b: "Hyper Text Markup Language",
+            c: "Home Tool Markup Language"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "2.Choose the correct HTML element to define important text",
+        answers: {
+            a: "<b>",
+            b: "<strong>",
+            c: "<important>"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "3.How can you make a numbered list?",
+        answers: {
+            a: "<ol>",
+            b: "<list>",
+            c: "<ul>"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "4.What does CSS stand for?",
+        answers: {
+            a: "Cascading Style Sheets",
+            b: "Computer Style Sheets",
+            c: "Colorful Style Sheets"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "5.Which property is used to change the background color?",
+        answers: {
+            a: "color",
+            b: "bgcolor",
+            c: "background-color"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "6.Which CSS property controls the text size?",
+        answer: {
+            a: "text-size",
+            b: "text-style",
+            c: "font-size"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "7.How do you select elements with class name 'test'?",
+        answer: {
+            a: "#test",
+            b: ".test",
+            c: "*test"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "8.Where is the correct place to insert a JavaScript?",
+        answer: {
+            a: "Both the <head> section and the <body> section are correct",
+            b: "The <head> section",
+            c: "The <body> section"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: '9.How do you write "Hello World" in an alert box?',
+        answer: {
+            a: 'alertBox("Hello World");',
+            b: 'alert("Hello Wrold")',
+            c: 'msg("Hello World")'
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "10.What is the correct way to write a JavaScript array?",
+        answer: {
+            a: 'var colors = ["red", "green", "blue"]',
+            b: 'var colors = (1:"red", 2:"green", 3:"blue")',
+            c: 'var colors = "red", "green", "blue"'
+        },
+        correctAnswer: "a"
+    }
+]
+
+
+
+
+// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+// 	function showQuestions(questions, quizContainer){
+// 		// code will go here
+// 	}
+
+// 	function showResults(questions, quizContainer, resultsContainer){
+// 		// code will go here
+// 	}
+
+// 	// show the questions
+// 	showQuestions(questions, quizContainer);
+
+// 	// when user clicks submit, show results
+// 	submitButton.onclick = function(){
+// 		showResults(questions, quizContainer, resultsContainer);
+// 	}
+// }
+
+
+// var myQuestions = [
+// 	{
+// 		question: "What is 10/2?",
+// 		answers: {
+// 			a: '3',
+// 			b: '5',
+// 			c: '115'
+// 		},
+// 		correctAnswer: 'b'
+// 	},
+// 	{
+// 		question: "What is 30/3?",
+// 		answers: {
+// 			a: '3',
+// 			b: '5',
+// 			c: '10'
+// 		},
+// 		correctAnswer: 'c'
+// 	}
+// ];
